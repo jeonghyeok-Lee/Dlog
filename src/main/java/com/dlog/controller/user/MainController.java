1b06f6bc43c7424a83aad1df3b338538852cc480
jeonghyeok.lee <jeonghyeok.lee.dev@gmail.com>
2022-12-09 01:23:37 +0900

    chore(MainController):Setting for Notice Output
    메인 페이지에서 공지사항을 출력하기 위해
    컨트롤러를 수정하였습니다.
    추가로 공지사항페이지는 사용자에서도 관찰이 가능하므로,
    어드민 컨트롤러에서 기본 컨트롤러로 이동

--- src/main/java/com/dlog/controller/user/MainController.java
+++ src/main/java/com/dlog/controller/user/MainController.java
@@ -1,5 +1,6 @@
 package com.dlog.controller.user;
 import com.dlog.service.MainService;
+import com.dlog.service.admin.NoticeService;
 import com.dlog.vo.user.UserVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.inject.Inject;
 
 
 // dispathcer-servlet에서 받은 요청을 처리할 핸들러 선택
@@ -17,9 +20,14 @@ public class MainController {
     @Qualifier("MainServiceImpl")
     private MainService mainService = null;
 
+    @Inject
+    @Qualifier("NoticeServiceImpl")
+    private NoticeService noticeService;
+
     //메인 페이지
     @GetMapping("/")
-    public String mainPage(){
+    public String mainPage(Model model) throws Exception{
+        model.addAttribute("noticeList",noticeService.getNoticeList());
         return "index";
     }
 
@@ -49,6 +57,13 @@ public class MainController {
     public String dbConnection(){
         System.out.println("db에 접근함" + mainService.getDbTime());
         return "index";
+    }
+
+
+    @GetMapping("/noticeList")
+    public String notice(Model model) throws Exception {
+        model.addAttribute("noticeList",noticeService.getNoticeList());
+        return "admin/notice/noticeList";
     }
 
 }
